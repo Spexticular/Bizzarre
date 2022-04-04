b2e24d7eb2cd8f9ff31e0dfcefdcc78453ecf2c2
Spexticular <jasont4608@gmail.com>
2022-04-04 10:29:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import pygame
+from time import sleepa as wait
 
+#Initallization of pygame
+pygame.init()
+
+# Making the window
+win = pygame.display.set_mode((1200, 752))
+bg = pygame.image.load("red.jpg")
+win.blit(bg, (0, 0))
+
+
+#QoL functions
+def flp():
+  pygame.display.update()
+def drawrect(x, y, width, height):
+  pygame.draw.rect((x, y, width, height))
