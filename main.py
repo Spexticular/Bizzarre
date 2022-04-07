794bab5cff8ae377556195e33dd0fdf3b64d11d9
Spexticular <jasont4608@gmail.com>
2022-04-07 11:38:23 -0400

    added jumping

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, webbrowser
 from time import sleep as wait
 
 #Initallization of pygame
@@ -8,13 +8,6 @@ pygame.init()
 def flp():
   pygame.display.update()
 
-# Making the window
-win = pygame.display.set_mode((1200, 752))
-win.fill((0, 0, 0))
-bg = pygame.image.load("red.jpg")
-win.blit(bg, (0, 0))
-flp()
-
 # Some values
 speed = 5
 grav = 5
@@ -23,6 +16,7 @@ level = 1
 lives = 5
 isjump = False
 isfall = False
+play = False
 
 # Making the player
 # Player Sprites
@@ -48,4 +42,63 @@ class player(object):
       self.image = playerleft
     if isfall:
       self.y += grav
-flp()
+# Making the main menu
+def mainmenu():
+  pygame.display.set_mode(())
+  
+# Main game loop and a check.
+if play:
+  # Making the window
+  win = pygame.display.set_mode((1200, 752))
+  win.fill((0, 0, 0))
+  bg = pygame.image.load("red.jpg")
+  win.blit(bg, (0, 0))
+  flp()
+  # Making the player
+  # Player Sprites
+  playersprite = pygame.image.load('player-removebg-preview.png')
+  playerleft = pygame.image.load('player-left-removebg-preview.png')
+
+  # Player Class
+  class player(object):
+    def __init__(self):
+      self.image = playersprite
+      self.xcor = 0
+      self.ycor = 0
+      self.rect = self.image.get_rect()
+      self.rect.topleft = [self.xcor, self.ycor]
+    def move(self):
+      # This variable returns true on whatever key is currently being pressed
+      key = pygame.key.get_pressed()
+      # Checking if any directional keys that we're using to control the plater are being held
+      if key[pygame.K_RIGHT]:
+        self.x += speed
+      if key[pygame.K_LEFT]:
+        self.x -= speed
+        self.image = playerleft
+      if isfall:
+        if not isjump:
+          self.y += grav
+      if isjump:
+        if not isfall:
+          # Making the variables for the jump
+          v=10
+          m=1
+          # The formula to make a smooth jump
+          F = (1/2)*m*(v**2)
+          # adding this value to the Y cordinate
+          y -= F
+          # changing the vel so that eventually you come back down
+          v -= 1
+          # Some shenanigans to make the jump better
+          if v < 0:
+            m = -1
+          # Reset when jump is finished
+          if v == -11:
+            isjump = False
+            isfall = False
+            v = 10
+            m = 1
+    def draw(self, screen):
+      screen.blit(self.image, self.rect)
+  pl = player()
