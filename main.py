cada3c4fd17844b35cef430cd36c11a7b448ecb6
Spexticular <jasont4608@gmail.com>
2022-04-07 09:59:16 -0400

    Gravity became a thing, kinda...

--- main.py
+++ main.py
@@ -16,17 +16,31 @@ win.blit(bg, (0, 0))
 flp()
 
 # Some values
+speed = 5
 grav = 5
 screen = 1
 level = 1
 lives = 5
+playersprite = pygame.image.load('player.png')
+isjump = False
+isfall = False
 
 # Making the player
-player = pygame.image.load("player.png")
-class player(pygame.sprite.Sprite):
+class player(object):
   def __init__(self):
-      self.xcor = 100
-      self.ycor = 150
-      self.image = pygame.image.load('player.png')
-      self.Rect(self.xcor, self.ycor, 100, 100)
+    self.image = playersprite
+    self.xcor = 0
+    self.ycor = 0
+    self.rect = self.image.get_rect()
+    self.rect.topleft = [self.xcor, self.ycor]
+  def move(self):
+    # This variable returns true on whatever key is currently being pressed
+    key = pygame.key.get_pressed()
+    # Checking if any directional keys that we're using to control the plater are being held
+    if key[pygame.K_RIGHT]:
+      self.x += speed
+    if key[pygame.K_LEFT]:
+      self.x -= speed
+    if isfall:
+      self.y += grav
 flp()
